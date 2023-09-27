6b9f0876ef3165f817573976368d510d379577cd
DebanjanBera <144529071+DebanjanBera@users.noreply.github.com>
2023-09-27 23:43:02 +0530

    Add files via upload
    This is an example of default constructor in java.

---
+++ default_c.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class default_c {
+    int a,b;
+    default_c(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the first number");
+        a=sc.nextInt();
+        System.out.println("enter the second number");
+        b=sc.nextInt();
+        System.out.println("addition value is"+(a+b));
+    }
+    public static void main(String[]args){
+         default_c dd=new default_c();
+    
+}
+
+    }
+
