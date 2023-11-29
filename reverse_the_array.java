896dcca1db2835e3dc1394c645ea02856b291161
DebanjanBera <144529071+DebanjanBera@users.noreply.github.com>
2023-11-29 23:38:33 +0530

    Add files via upload

---
+++ reverse_the_array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class reverse_the_array {
+    public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int j=0;
+int []a=new int[5];
+int []b=new int[5];
+for (int i=0;i<5;i++){
+    System.out.println("enter the number");
+    a[i]=sc.nextInt();
+}
+
+for(int k=a.length-1;j=0;k>=0;k=k-1;j++){
+    
+}
+    }
+    
+}
