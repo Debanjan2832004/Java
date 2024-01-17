360d2afb470dfc74964628c1db610f51ecf7173e
Debanjan Bera <144529071+Debanjan2832004@users.noreply.github.com>
2024-01-18 00:18:58 +0530

    Add files via upload

---
+++ perimeter_circle.java
@@ -0,0 +1,9 @@
+import java .util.*;
+public class perimeter_circle {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the radius value");
+        int r=sc.nextInt();
+        System.out.println("perimeter of circle is"+(2*3.14*r));
+    }
+}
