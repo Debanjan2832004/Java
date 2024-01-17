6e54d923c9dd81d78775e46cc130248c5655f732
Debanjan Bera <144529071+Debanjan2832004@users.noreply.github.com>
2024-01-18 00:21:25 +0530

    Add files via upload

--- thread.java
+++ thread.java
@@ -1,11 +1,16 @@
 // THREAD IMPLEMENTETION BY EXTENDING Thread CLASS
 class b extends Thread {
 public void run(){
-for (int i=0;i<5;i++){
+for (int i=0;i<101;i++){
     System.out.println("Debanjan");
     try {
-        Thread.sleep(1000);
+        Thread.sleep(100000);
     } catch (InterruptedException e) {}
+    int n=0;
+    while (n<5) {
+        System.out.println("hello world");
+        n++;
+    }
 }
     }
 }
@@ -14,7 +19,11 @@ for (int i=0;i<5;i++){
  class thread{
     public static void main(String[] args) {
         b tt=new b();
+        //tt.start();
         tt.start();
+         b tt1=new b();
+        //tt.start();
+        tt1.start();
         for (int i=0;i<5;i++){
     System.out.println("Deb");
     try{
